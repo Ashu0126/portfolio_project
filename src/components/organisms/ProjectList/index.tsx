1878900bc6466871a928a3e684c4963bcea4aa72
Ashu0126 <ashish.jaryal14@gmail.com>
2024-02-15 13:12:51 +0530

    loader added

--- src/components/organisms/ProjectList/index.tsx
+++ src/components/organisms/ProjectList/index.tsx
@@ -1,37 +1,51 @@
-import { Fragment } from "react";
+"use client";
+import { Fragment, useEffect, useState } from "react";
 import ProjectCard from "../../molecules/ProjectCard";
 import Navbar from "../../molecules/Navbar";
 import style from "./index.module.scss";
 import pageData from "./../../../data/projects.json";
+import Loader from "../../atoms/Loader";
 
 const ProjectList = () => {
+  const [showCard, setShowCard] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setShowCard(true);
+    }, 1500);
+  }, []);
+
   return (
     <div className={style.projects}>
       <Navbar />
       <h1>{pageData?.heading}</h1>
-      <div className={style.projectCards}>
-        {pageData?.projects?.map(
-          (project: {
-            name: string;
-            img: string;
-            description: string;
-            webLink?: string;
-            github?: string;
-          }) => (
-            <Fragment key={project?.name}>
-              <ProjectCard
-                name={project?.name}
-                projectImg={project?.img}
-                description={project?.description}
-                webLink={project?.webLink}
-                githubLink={project?.github}
-                githubIcon={pageData?.githubIcon}
-                githubText={pageData?.githubText}
-                descriptionText={pageData?.description}
-                liveIcon={pageData?.liveIcon}
-                liveText={pageData?.liveText}
-              />
-            </Fragment>
+      <div className={!showCard ? style.loader : style.projectCards}>
+        {!showCard ? (
+          <Loader />
+        ) : (
+          pageData?.projects?.map(
+            (project: {
+              name: string;
+              img: string;
+              description: string;
+              webLink?: string;
+              github?: string;
+            }) => (
+              <Fragment key={project?.name}>
+                <ProjectCard
+                  name={project?.name}
+                  projectImg={project?.img}
+                  description={project?.description}
+                  webLink={project?.webLink}
+                  githubLink={project?.github}
+                  githubIcon={pageData?.githubIcon}
+                  githubText={pageData?.githubText}
+                  descriptionText={pageData?.description}
+                  liveIcon={pageData?.liveIcon}
+                  liveText={pageData?.liveText}
+                />
+              </Fragment>
+            )
           )
         )}
       </div>
