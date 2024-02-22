53ca4fc28ce5de1c3bfab77ebfbb5f399b7c3ddf
Ashu0126 <ashish.jaryal14@gmail.com>
2024-02-22 20:19:17 +0530

    navbar done

--- src/components/molecules/Navbar/index.tsx
+++ src/components/molecules/Navbar/index.tsx
@@ -4,10 +4,15 @@ import style from "./index.module.scss";
 
 const Navbar = () => {
   const [isHome, setIsHome] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
 
   useEffect(() => {
     setIsHome(window?.location?.pathname === "/");
   }, []);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
 
   return (
     <div className={style.navbar}>
@@ -18,11 +23,28 @@ const Navbar = () => {
       ) : (
         <h3>PORTFOLIO</h3>
       )}
-      <ul>
-        <li>ABOUT</li>
-        <li>PROJECTS</li>
-        <li>HIRE ME</li>
-      </ul>
+      <img
+        className={style.menuIcon}
+        onClick={toggleMenu}
+        src="https://static-00.iconduck.com/assets.00/hamburger-menu-icon-2048x1536-6qrrvtw1.png"
+        alt=""
+      />
+      <div className={`${style.navItem} ${openMenu ? style.navOpen : ""}`}>
+        <div className={style.heading}>
+          <h4>PORTFOLIO</h4>
+          <img
+            onClick={toggleMenu}
+            src="https://cdn-icons-png.freepik.com/512/222/222360.png"
+            alt=""
+          />
+        </div>
+        <ul>
+          <li>ABOUT</li>
+          <li>PROJECTS</li>
+          <li>RESUME</li>
+          <li>HIRE ME</li>
+        </ul>
+      </div>
     </div>
   );
 };
